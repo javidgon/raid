f5a4d884abf9986dcbfa778ce9915c2dedcdcc54
Jose Vidal <javidgon@gmail.com>
2013-01-10 18:12:26 +0100

    Create classes and PEP-8 compliance

--- raid.py
+++ raid.py
@@ -5,36 +5,49 @@ import sys
 from optparse import OptionParser
 
 
-def controller(url, concurrency, requests_number):
-    if validate_url(url):
-        create_worker(url=url, concurrency=concurrency,
-                      requests_number=requests_number)
-    
-def create_worker(url, concurrency, requests_number):
-    worker = 1
-    workers = []
-    while worker <= concurrency:
-        workers.append(subprocess.Popen('python worker.py %s %s %s' %
-                                        (worker, url, requests_number), shell=True))
-        worker += 1
-        
-    while True:
-        if all([x.poll() for x in workers]):
-            print "Switching off workers..."
+class Raid(object):
+
+    def __call__(self, url, concurrency, requests_number):
+        self.controller(url, concurrency, requests_number)
+
+    def controller(self, url, concurrency, requests_number):
+        if self.validate_url(url):
+            self.create_worker(url=url, concurrency=concurrency,
+                               requests_number=requests_number)
+        else:
+            print "[Controller] Invalid URL"
             sys.exit(1)
 
-def validate_url(url):
-    return urlparse.urlparse(url)
+    def create_worker(self, url, concurrency, requests_number):
+        worker = 1
+        workers = []
+        while worker <= concurrency:
+            workers.append(subprocess.Popen('python worker.py %s %s %s' %
+                           (worker, url, requests_number),
+                           shell=True))
+            worker += 1
+
+        while True:
+            if all([x.poll() for x in workers]):
+                print "[Controller] Switching off workers..."
+                sys.exit(1)
+
+    def validate_url(self, url):
+        parts = urlparse.urlparse(url)
+        return parts.scheme in ('http', 'https')
 
 
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option("-u", "--url", action="store", type="string",
-                      default="http://127.0.0.1:8000", help="Requests' target", dest="url")
+                      default="http://127.0.0.1:8000",
+                      help="Requests' target", dest="url")
     parser.add_option("-w", "--workers", action="store", type="int",
                       default=1, help="Number of workers", dest="workers")
     parser.add_option("-r", "--requests", action="store", type="int",
-                      default=1, help="Number of requests per worker", dest="requests_number")
+                      default=1, help="Number of requests per worker",
+                      dest="requests_number")
     (options, args) = parser.parse_args()
-        
-    controller(options.url, options.workers, options.requests_number)
+
+    raid = Raid()
+    raid(options.url, options.workers, options.requests_number)
--- worker.py
+++ worker.py
@@ -1,16 +1,25 @@
 import requests
 import sys
 
-def make_request(worker, url, requests_number):
-    for i in range(1, int(requests_number) + 1):
-        print '[Worker %s - %s time] Making the request to %s' % (worker, i, url)
-        try:
-            result = requests.get(url)
-        except requests.exceptions.ConnectionError as e:
-            print '[Worker %s - %s time] Got %s!' % (worker, i, e)
-            sys.exit(1)
-        print '[Worker %s - %s time] Request finalized with status %s' % (worker, i, result.status_code)
-    sys.exit(1)
-        
+
+class Worker(object):
+
+    def __call__(self, worker, url, requests_number):
+        self.make_request(worker, url, requests_number)
+
+    def make_request(self, worker, url, requests_number):
+        for i in range(1, int(requests_number) + 1):
+            print ('[Worker %s - %s time] Making the request to %s'
+                   % (worker, i, url))
+            try:
+                result = requests.get(url)
+            except requests.exceptions.ConnectionError as e:
+                print '[Worker %s - %s time] Got %s!' % (worker, i, e)
+                sys.exit(1)
+            print ('[Worker %s - %s time] Request finalized with status %s'
+                   % (worker, i, result.status_code))
+        sys.exit(1)
+
 if __name__ == '__main__':
-    make_request(sys.argv[1], sys.argv[2], sys.argv[3])
+    worker = Worker()
+    worker(sys.argv[1], sys.argv[2], sys.argv[3])
