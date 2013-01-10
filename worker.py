19cce6a7b704811176b8573f325c51a1e380e361
Jose Vidal <javidgon@gmail.com>
2013-01-10 17:44:05 +0100

    Create worker.py
    This file represents the worker, the one who makes the request
    to a concrete url a shows up the result

---
+++ worker.py
@@ -0,0 +1,16 @@
+import requests
+import sys
+
+def make_request(worker, url, requests_number):
+    for i in range(0, int(requests_number)):
+        print '[Worker %s - %s time] Making the request to %s' % (worker, i, url)
+        try:
+            result = requests.get(url)
+        except requests.exceptions.ConnectionError as e:
+            print '[Worker %s - %s time] Got %s!' % (worker, i, e)
+            sys.exit(1)
+        print '[Worker %s - %s time] Request finalized with status %s' % (worker, i, result.status_code)
+    sys.exit(1)
+        
+if __name__ == '__main__':
+    make_request(sys.argv[1], sys.argv[2], sys.argv[3])
