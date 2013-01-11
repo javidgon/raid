6b38e17615b93911a309594ff1d1c73fd003d2d2
Jose Vidal <javidgon@gmail.com>
2013-01-11 11:01:24 +0100

    Make worker.py vars' names more obvious

--- worker.py
+++ worker.py
@@ -4,24 +4,29 @@ import sys
 
 class Worker(object):
 
-    def __call__(self, worker, url, requests_number):
-        self.make_request(worker, url, requests_number)
+    def __call__(self, worker_id, url, requests_number):
+        self.make_request(worker_id, url, requests_number)
 
-    def make_request(self, worker, url, requests_number):
+    def make_request(self, worker_id, url, requests_number):
         """
         It's the worker's job, make requests. It makes the
         requests sequentially.
+        
+        :param worker_id: Worker's id.
+        :param url: Requests' target.
+        :param requests_number: Number of requests per worker. Define
+                                sequentiality.
         """
         for i in range(1, int(requests_number) + 1):
             print ('[Worker %s - %s time] Making the request to %s'
-                   % (worker, i, url))
+                   % (worker_id, i, url))
             try:
                 result = requests.get(url)
             except requests.exceptions.ConnectionError as e:
-                print '[Worker %s - %s time] Got %s!' % (worker, i, e)
+                print '[Worker %s - %s time] Got %s!' % (worker_id, i, e)
                 sys.exit(1)
             print ('[Worker %s - %s time] Request finalized with status %s'
-                   % (worker, i, result.status_code))
+                   % (worker_id, i, result.status_code))
         sys.exit(1)
 
 if __name__ == '__main__':
